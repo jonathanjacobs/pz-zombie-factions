1b89f517a910a9a40b6593e6c3958df80a03e657
Jonathan Jacobs <jonathan.jacobs@gmail.com>
2026-08-26 23:09:34 -0400

    Extend admin Horde Spawning with faction test controls

---
+++ Contents/mods/pz-zombie-factions/42/media/lua/client/ZombieFactions/HordeSpawnerExtension_Client.lua
@@ -0,0 +1,154 @@
+require "DebugUIs/ISSpawnHordeUI"
+require "ZombieFactions/Constants"
+
+local MODULE = "ZombieFactions"
+local VANILLA = ZombieFactions.Faction.VANILLA
+local REL = ZombieFactions.Relationship
+
+local originalCreateChildren = ISSpawnHordeUI.createChildren
+local originalOnSpawn = ISSpawnHordeUI.onSpawn
+
+local function addRelationshipOptions(combo)
+    combo:addOptionWithData("FRIENDLY", REL.FRIENDLY)
+    combo:addOptionWithData("NEUTRAL", REL.NEUTRAL)
+    combo:addOptionWithData("HOSTILE", REL.HOSTILE)
+end
+
+local function selectedData(combo)
+    local option = combo and combo.options and combo.options[combo.selected]
+    return option and option.data or nil
+end
+
+local function moveDown(control, amount)
+    if control then
+        control:setY(control:getY() + amount)
+    end
+end
+
+function ISSpawnHordeUI:createChildren()
+    originalCreateChildren(self)
+
+    local fontHeight = getTextManager():getFontHeight(UIFont.Small)
+    local rowHeight = fontHeight + 6
+    local spacing = 10
+    local extraHeight = (rowHeight + spacing) * 4
+    local x = 11
+    local y = self.healthSlider:getBottom() + spacing
+
+    self:setHeight(self:getHeight() + extraHeight)
+    moveDown(self.add, extraHeight)
+    moveDown(self.closeButton2, extraHeight)
+    moveDown(self.removezombies, extraHeight)
+    moveDown(self.clearbodies, extraHeight)
+
+    self.zfFactionLabel = ISLabel:new(x, y, rowHeight, "Zombie faction:", 1, 1, 1, 1, UIFont.Small, true)
+    self:addChild(self.zfFactionLabel)
+
+    self.zfFaction = ISComboBox:new(self.zfFactionLabel:getRight() + spacing, y, 180, rowHeight)
+    self.zfFaction:initialise()
+    self:addChild(self.zfFaction)
+    self.zfFaction:addOptionWithData("Vanilla (zf:vanilla)", VANILLA)
+    self.zfFaction:addOptionWithData("Test Red (zf:test-red)", "zf:test-red")
+    self.zfFaction:addOptionWithData("Test Blue (zf:test-blue)", "zf:test-blue")
+
+    y = y + rowHeight + spacing
+    self.zfToVanillaLabel = ISLabel:new(x, y, rowHeight, "Spawned faction -> Vanilla:", 1, 1, 1, 1, UIFont.Small, true)
+    self:addChild(self.zfToVanillaLabel)
+
+    self.zfToVanilla = ISComboBox:new(self.zfToVanillaLabel:getRight() + spacing, y, 140, rowHeight)
+    self.zfToVanilla:initialise()
+    self:addChild(self.zfToVanilla)
+    addRelationshipOptions(self.zfToVanilla)
+
+    y = y + rowHeight + spacing
+    self.zfFromVanillaLabel = ISLabel:new(x, y, rowHeight, "Vanilla -> spawned faction:", 1, 1, 1, 1, UIFont.Small, true)
+    self:addChild(self.zfFromVanillaLabel)
+
+    self.zfFromVanilla = ISComboBox:new(self.zfFromVanillaLabel:getRight() + spacing, y, 140, rowHeight)
+    self.zfFromVanilla:initialise()
+    self:addChild(self.zfFromVanilla)
+    addRelationshipOptions(self.zfFromVanilla)
+
+    y = y + rowHeight + spacing
+    self.zfSymmetric = ISTickBox:new(x, y, 300, rowHeight, "", self, nil)
+    self.zfSymmetric:initialise()
+    self:addChild(self.zfSymmetric)
+    self.zfSymmetric:addOption("Symmetric: mirror first relationship both ways")
+    self.zfSymmetric.selected[1] = true
+end
+
+local function buildFactionSpawnArgs(self, factionId)
+    local femaleChance = nil
+    local outfit = self:getOutfit()
+    if outfit and self.maleOutfits:contains(outfit) and not self.femaleOutfits:contains(outfit) then
+        femaleChance = 0
+    elseif outfit and self.femaleOutfits:contains(outfit) and not self.maleOutfits:contains(outfit) then
+        femaleChance = 100
+    end
+
+    local selected = self.boolOptions.selected
+    return {
+        x = self.selectX,
+        y = self.selectY,
+        z = self.selectZ,
+        count = self:getZombiesNumber(),
+        radius = self:getRadius(),
+        outfit = outfit or "",
+        femaleChance = femaleChance,
+        knockedDown = selected[1] == true,
+        crawler = selected[2] == true,
+        isFakeDead = selected[3] == true,
+        isFallOnFront = selected[4] == true,
+        isInvulnerable = selected[5] == true,
+        isSitting = selected[6] == true,
+        isRecordingAnims = selected[7] == true,
+        isRagdolling = selected[8] == true,
+        onFire = selected[9] == true,
+        health = self.healthSlider:getCurrentValue(),
+        heightOffset = self:getHeightOffset(),
+        factionId = factionId,
+        toVanilla = selectedData(self.zfToVanilla) or REL.FRIENDLY,
+        fromVanilla = selectedData(self.zfFromVanilla) or REL.FRIENDLY,
+        symmetric = self.zfSymmetric.selected[1] == true,
+    }
+end
+
+function ISSpawnHordeUI:onSpawn()
+    local factionId = selectedData(self.zfFaction) or VANILLA
+
+    if factionId == VANILLA then
+        return originalOnSpawn(self)
+    end
+
+    if not isClient() then
+        print("[ZombieFactions] faction Horde Spawning harness currently requires multiplayer/server authority")
+        return
+    end
+
+    local player = getPlayer()
+    if not player then
+        print("[ZombieFactions] cannot spawn faction test horde: no local player")
+        return
+    end
+
+    sendClientCommand(player, MODULE, "SpawnTestHorde", buildFactionSpawnArgs(self, factionId))
+end
+
+local function onServerCommand(module, command, args)
+    if module ~= MODULE or command ~= "SpawnTestHordeResult" then return end
+    args = args or {}
+    if args.ok then
+        print(string.format(
+            "[ZombieFactions][%s] %s: spawned %s/%s as %s",
+            tostring(args.runId or "SPIKE001"),
+            tostring(args.message or "spawn complete"),
+            tostring(args.spawned or "?"),
+            tostring(args.requested or "?"),
+            tostring(args.factionId or "?")
+        ))
+    else
+        print("[ZombieFactions] faction horde spawn rejected: " .. tostring(args.message or "unknown error"))
+    end
+end
+
+Events.OnServerCommand.Add(onServerCommand)
