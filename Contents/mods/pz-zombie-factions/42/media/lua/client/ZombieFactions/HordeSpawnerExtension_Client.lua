a5618791ba6776598007a32401020cd03ca19860
Jonathan Jacobs <jonathan.jacobs@gmail.com>
2026-08-27 00:05:16 -0400

    Fix Horde Spawner bottom button layout

--- Contents/mods/pz-zombie-factions/42/media/lua/client/ZombieFactions/HordeSpawnerExtension_Client.lua
+++ Contents/mods/pz-zombie-factions/42/media/lua/client/ZombieFactions/HordeSpawnerExtension_Client.lua
@@ -10,6 +10,8 @@ local REL = ZombieFactions.Relationship
 local originalCreateChildren = ISSpawnHordeUI.createChildren
 local originalOnSpawn = ISSpawnHordeUI.onSpawn
 
+print("[ZombieFactions] Client Horde Spawner extension loaded v0.0.3")
+
 local function addRelationshipOptions(combo)
     combo:addOptionWithData("FRIENDLY", REL.FRIENDLY)
     combo:addOptionWithData("NEUTRAL", REL.NEUTRAL)
@@ -31,16 +33,10 @@ function ISSpawnHordeUI:createChildren()
     local x = 11
     local y = self.healthSlider:getBottom() + spacing
 
-    local addY = self.add and self.add:getY() or nil
-    local closeY = self.closeButton2 and self.closeButton2:getY() or nil
-    local removeY = self.removezombies and self.removezombies:getY() or nil
-    local bodiesY = self.clearbodies and self.clearbodies:getY() or nil
-
+    -- The vanilla bottom buttons are anchorBottom=true. Increasing the window
+    -- height automatically moves them down by the same delta. Do not manually
+    -- reposition them here or they will be shifted below the visible window.
     self:setHeight(self:getHeight() + extraHeight)
-    if addY then self.add:setY(addY + extraHeight) end
-    if closeY then self.closeButton2:setY(closeY + extraHeight) end
-    if removeY then self.removezombies:setY(removeY + extraHeight) end
-    if bodiesY then self.clearbodies:setY(bodiesY + extraHeight) end
 
     self.zfFactionLabel = ISLabel:new(x, y, rowHeight, "Zombie faction:", 1, 1, 1, 1, UIFont.Small, true)
     self:addChild(self.zfFactionLabel)
