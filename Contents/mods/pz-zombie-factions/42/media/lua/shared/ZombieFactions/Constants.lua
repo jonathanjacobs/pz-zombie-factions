5e0f0600fa885c650a0cebf72dbea8c41cae8904
Jonathan Jacobs <jonathan.jacobs@gmail.com>
2026-08-26 22:47:42 -0400

    Add faction constants

---
+++ Contents/mods/pz-zombie-factions/42/media/lua/shared/ZombieFactions/Constants.lua
@@ -0,0 +1,15 @@
+ZombieFactions = ZombieFactions or {}
+
+ZombieFactions.Relationship = {
+    FRIENDLY = "FRIENDLY",
+    NEUTRAL = "NEUTRAL",
+    HOSTILE = "HOSTILE",
+}
+
+ZombieFactions.Faction = {
+    VANILLA = "zf:vanilla",
+}
+
+ZombieFactions.PlayerTarget = {
+    UNFACTIONED = "pf:unfactioned",
+}
